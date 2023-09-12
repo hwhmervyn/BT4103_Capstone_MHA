254e92b0547170ffe9893b546107b76d1da22652
Mingg-Shi <114652173+Mingg-Shi@users.noreply.github.com>
2023-09-12 14:56:49 +0800

    .

--- pdfSections.py
+++ pdfSections.py
@@ -15,11 +15,23 @@ def keepFromTitle(spans):
     #print(f"removed {b4-len(block_font)} lines")
     return spans
 
+def stringEndsOnFullStop(text):
+   trailingSpacesRmved = text.strip()
+   if len(trailingSpacesRmved) == 0: return False
+   else: return trailingSpacesRmved.strip()[-1] != '.'
+
+def isDiffSection(prevSpanFontSize, fontSize, prevSpanFont, font):
+  conditions = [
+      prevSpanFontSize != fontSize,
+      prevSpanFontSize == fontSize and prevSpanFont != font
+  ]
+  return sum(conditions) > 0
+
 def aggregateSpansToSections(spans):
     sections = []
 
     prevSpan = spans[0]
-    prevSpanText = [prevSpan['text']] #only concat text at the end of each section
+    prevSectionText = [prevSpan['text']] #only concat text at the end of each section
     prevSpanFontSize = round(prevSpan['size'])
     prevSpanFont = prevSpan['font']
     section_num = 0
@@ -29,20 +41,20 @@ def aggregateSpansToSections(spans):
       spanText = span['text']
       fontSize = round(span['size'])
       font = span['font']
-
-      if prevSpanFontSize != fontSize or (prevSpanFontSize == fontSize and prevSpanFont != font and '.' != prevSpanText[-1].strip()):
-        section_text = ' '.join(prevSpanText)#.replace('\n','')
+      
+      if isDiffSection(prevSpanFontSize, fontSize, prevSpanFont, font):
+        section_text = ' '.join(prevSectionText)#.replace('\n','')
         sections.append(section_text)
         section_num += 1
 
         prevSpan = span
-        prevSpanText = [spanText]
+        prevSectionText = [spanText]
         prevSpanFontSize =  fontSize
         prevSpanFont = font
 
       else:
-        prevSpanText.append(spanText)
+        prevSectionText.append(spanText)
 
-    section_text = ' '.join(prevSpanText)#.replace('\n','')
+    section_text = ' '.join(prevSectionText)#.replace('\n','')
     sections.append(section_text)
     return sections
