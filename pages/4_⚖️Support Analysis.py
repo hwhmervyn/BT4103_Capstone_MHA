ecf29c720530df37a7f92a93bad0cda642ad0ecb
laitzheng <laitzheng@gmail.com>
2023-10-11 17:39:55 +0800

    Link to filtered articles output. Add process checks for support analysis.

--- pages/4_⚖️Support Analysis.py
+++ pages/4_⚖️Support Analysis.py
@@ -12,6 +12,7 @@ sys.path.append(os.path.join(os.getcwd(), "cost_breakdown"))
 
 from chromaUtils import getCollection, getDistinctFileNameList
 from hypSupport import get_llm_response, get_stance_and_evidence, get_support_chart, get_support_table
+from Individual_Analysis import get_yes_pdf_filenames
 from update_cost import update_usage_logs
 
 st.set_page_config(layout="wide")
@@ -21,71 +22,97 @@ st.markdown("<h1 style='text-align: left; color: Black;'>Support Analysis</h1>",
 st.markdown('#')
 
 input = st.text_input("Research Prompt", placeholder='Enter your prompt (e.g. Is drug A more harmful than drug B?)')
-if st.button("Analyse literature support"):
-    with get_openai_callback() as usage_info:
-        # Obtain dataframe for LLM response
-        # Connect to database
-        db = getCollection("pdf")
+col1, col2 = st.columns(2)
+with col1: 
+    start_analysis = st.button("Analyse literature support")
+with col2:
+    analyse_all_articles = st.toggle("All articles", value=False, 
+                                     help="Select the toggle to analyse all uploaded articles. If not selected, analysis will only be conducted on filtered articles.")
 
-        # Initialise holder lists
+# Run if "Analyse literature support" button is clicked
+if start_analysis:
+    # Run if user has included a prompt
+    if input:
+        # Initialise holder lists to temporarily store output
         stance_list = []
         evidence_list = []
         source_docs_list = []
-        article_title_list = getDistinctFileNameList("pdf")[:3] # RESTRICT TO 3 FOR TESTING
-        total_num_articles = len(article_title_list)
+        article_title_list = []
+        total_num_articles = len(article_title_list) # initialised to 0
 
-        progressBar = st.progress(0, text="Analysing articles...")
+        # 2 options: Analyse only filtered PDF articles or all PDF articles
+        if not analyse_all_articles:
+            # Retrieve output dataframe of PDF analysis from Streamlit session state
+            if 'pdf_ind_fig2' not in st.session_state:
+                st.error("You have no filtered PDF articles")
+            else: 
+                # Extract output of PDF upload and filtering stage
+                ind_findings_df = st.session_state.pdf_ind_fig2
+                article_title_list = get_yes_pdf_filenames(ind_findings_df)[:3] # RESTRICT TO 3 FOR TESTING
+                total_num_articles = len(article_title_list)
+        else:
+            article_title_list = getDistinctFileNameList("pdf")[:3] # RESTRICT TO 3 FOR TESTING
+            total_num_articles = len(article_title_list)
+            if total_num_articles == 0:
+                st.error("You have no PDF articles in the database. Please use the PDF Analysis page to upload the articles.")
 
-        start_total = time.time()
-        for i in range(total_num_articles):
-            article_title = article_title_list[i]
-            start = time.time()
-            response, source_docs = get_llm_response(db, input, article_title)
-            end = time.time()
-            # Get stance and evidence. Append to holder lists
-            stance, evidence = get_stance_and_evidence(response)
-            stance_list.append(stance)
-            evidence_list.append(evidence)
-            source_docs_list.append(source_docs)
-            # Track time taken
-            time_taken = round(end - start, 0)
-            progress_display_text = f"Analysing articles: {i+1}/{total_num_articles} completed. Time taken: {time_taken}s."
-            progressBar.progress((i+1)/total_num_articles, text=progress_display_text)
-        
-        support_df = pd.DataFrame({"article": article_title_list, "stance": stance_list, "evidence": evidence_list, "source_docs": source_docs_list})
-        end_total = time.time()
-        
-        # Display success message
-        progressBar.empty()
-        time_taken_total = round((end_total - start_total)/60, 1)
-        st.success(f"Analysis Complete. Total time taken: {time_taken_total}min.")
-        
-        # TEST
-        # support_df = pd.read_csv("C:/Users/laitz/OneDrive/Documents/BT4103_Capstone_MHA/output/support_output [Is cannabis more harmful than tobacco_].csv")
+        with get_openai_callback() as usage_info:
+            # Connect to database
+            db = getCollection("pdf")
+    
+            # Obtain dataframe for LLM response
+            # Only run if number of articles is greater than 0
+            if total_num_articles > 0: 
+                progressBar = st.progress(0, text="Analysing articles...")
 
-        st.markdown('#')
-        st.text("Support Summary Chart:")
-        fig1 = get_support_chart(support_df)
-        fig1.update_layout(title_text='Distribution of article response')
-        st.plotly_chart(fig1, use_container_width=True)
+                start_total = time.time()
+                for i in range(total_num_articles):
+                    article_title = article_title_list[i]
+                    start = time.time()
+                    response, source_docs = get_llm_response(db, input, article_title)
+                    end = time.time()
+                    # Get stance and evidence. Append to holder lists
+                    stance, evidence = get_stance_and_evidence(response)
+                    stance_list.append(stance)
+                    evidence_list.append(evidence)
+                    source_docs_list.append(source_docs)
+                    # Track time taken
+                    time_taken = round(end - start, 0)
+                    progress_display_text = f"Analysing articles: {i+1}/{total_num_articles} completed. Time taken: {time_taken}s."
+                    progressBar.progress((i+1)/total_num_articles, text=progress_display_text)
+                
+                support_df = pd.DataFrame({"article": article_title_list, "stance": stance_list, "evidence": evidence_list, "source_docs": source_docs_list})
+                end_total = time.time()
+                
+                # Display success message
+                progressBar.empty()
+                time_taken_total = round((end_total - start_total)/60, 1)
+                st.success(f"Analysis Complete. Total time taken: {time_taken_total}min.")
+                
+                # TEST
+                # support_df = pd.read_csv("C:/Users/laitz/OneDrive/Documents/BT4103_Capstone_MHA/output/support_output [Is cannabis more harmful than tobacco_].csv")
 
-        st.text("Support Summary Table:")
-        fig2 = get_support_table(support_df)
-        fig2.update_layout(title_text='Article response and evidence', margin_autoexpand=True, height=800)
-        st.plotly_chart(fig2, use_container_width=True, )
+                st.markdown('#')
+                st.text("Support Summary Chart:")
+                fig1 = get_support_chart(support_df)
+                fig1.update_layout(title_text='Distribution of article response')
+                st.plotly_chart(fig1, use_container_width=True)
 
-        # TODO
-        # st.download_button(label="Download output dataframe (csv file)",
-        #                     # Store output results in a csv file
-        #                     data=support_df.to_csv(index=False),
-        #                     # Query appended at end of output file name
-        #                     file_name=f'support_output [{input}].csv',
-        #                     mime='text/csv')
-        
-        # except Exception as err:
-        #     st.error(f"Unexpected Error: {err}")
-        #     st.error(f"Error Type: {type(err)}")
+                st.text("Support Summary Table:")
+                fig2 = get_support_table(support_df)
+                fig2.update_layout(title_text='Article response and evidence', margin_autoexpand=True, height=800)
+                st.plotly_chart(fig2, use_container_width=True, )
 
-        # Update usage info
-        update_usage_logs("Support Analysis", input, 
-                          usage_info.prompt_tokens, usage_info.completion_tokens, usage_info.total_cost)
+                # TODO
+                # st.download_button(label="Download output dataframe (csv file)",
+                #                     # Store output results in a csv file
+                #                     data=support_df.to_csv(index=False),
+                #                     # Query appended at end of output file name
+                #                     file_name=f'support_output [{input}].csv',
+                #                     mime='text/csv')
+                
+                # Update usage info
+                update_usage_logs("Support Analysis", input, 
+                                usage_info.prompt_tokens, usage_info.completion_tokens, usage_info.total_cost)
+    else:
+        st.warning("Please input a prompt")
