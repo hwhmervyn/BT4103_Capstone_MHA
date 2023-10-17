78115b6dfb1cc8a67b021c28107f61e489d72a4d
ashleyleerx <ashleyleeruoxin@gmail.com>
2023-10-18 00:17:52 +0800

    Update Home Page

--- NewHomePage.py
+++ NewHomePage.py
@@ -6,14 +6,18 @@ import streamlit as st
 from streamlit_lottie import st_lottie 
 from streamlit_extras.switch_page_button import switch_page
 
+#Setting page config
+st.set_page_config(layout="centered")
 
+#Add logo
 add_logo("images/htpd_text.png", height=100)
 
-
+#Add animatioon
 path = "images/research_animation.json"
 with open(path,"r") as file: 
     url = json.load(file) 
 
+#CSS
 st.markdown("""
 <style>
 .research {
@@ -43,9 +47,11 @@ st.markdown("""
 </style>
 """, unsafe_allow_html=True)
 
+#Title
 st.markdown("<h1 style='text-align: center; padding-top: 0rem;'><p class='research'>research<p class='xpress'>Xpress</p></h1>", unsafe_allow_html=True)
 st.markdown("<h4 style='text-align: center; color: Black;'>Synthesize Research Evidence through AI Capabilities</h4>", unsafe_allow_html=True)
 
+#Animation
 st_lottie(url, 
     loop=True, 
     quality='high',
