a75202e7a84b907c9f32b03054fbc05db0ebeab6
ashleyleerx <ashleyleeruoxin@gmail.com>
2023-10-21 00:05:32 +0800

    Update 3_📗Excel Filtering.py

--- pages/3_📗Excel Filtering.py
+++ pages/3_📗Excel Filtering.py
@@ -1,18 +1,24 @@
 import streamlit as st
 from streamlit_extras.app_logo import add_logo
 from concurrent.futures import as_completed
+from langchain.callbacks import get_openai_callback
 import pandas as pd
 
 import sys,os
-chromaDirectory = os.path.join(os.getcwd(), "ChromaDB")
-costDirectory = os.path.join(os.getcwd(), "cost_breakdown")
-if chromaDirectory not in sys.path:
-    sys.path.append(chromaDirectory)
-if costDirectory not in sys.path:
-    sys.path.append(costDirectory)
+
+workingDirectory = os.getcwd()
+chromaDirectory = os.path.join(workingDirectory, "ChromaDB")
+miscellaneousDirectory = os.path.join(workingDirectory, "Miscellaneous")
+costDirectory = os.path.join(workingDirectory, "cost_breakdown")
+
+sys.path.append(chromaDirectory)
+sys.path.append(miscellaneousDirectory)
+sys.path.append(costDirectory)
 
 from filterExcel import filterExcel, getOutputDF, generate_visualisation, clean_findings_df
+from User_Input_Cleaning import process_user_input
 from update_cost import update_usage_logs, Stage
+
 
 st.set_page_config(layout="wide")
 add_logo("images/htpd_text.png", height=100)
@@ -43,25 +49,34 @@ if not st.session_state.filtered:
         elif not input or not uploaded_file:
             st.error("Please enter a research prompt and upload an excel file")
         else:
-            _, futures = filterExcel(uploaded_file,  input)
-            issues, results, numDone, numFutures, total_input_tokens, total_output_tokens, total_cost = [],[], 0, len(futures), 0, 0, 0
-            progessBar = st.progress(0, text="Article filtering in progress...")
-            for future in as_completed(futures):
-                row = future.result()
-                total_input_tokens += row[5]
-                total_output_tokens += row[6]
-                total_cost += row[7]
-                results.append(row[0:5])
-                numDone += 1
-                progessBar.progress(numDone/numFutures, text="Article filtering in progress...")
-            
-            update_usage_logs(Stage.EXCEL_FILTERING.value, input, total_input_tokens,total_output_tokens,total_cost)
-            dfOut = pd.DataFrame(results, columns = ["DOI","TITLE","ABSTRACT","llmOutput", "jsonOutput"])
-            dfOut = getOutputDF(dfOut)
-            dfOut.to_excel("output/excel_result.xlsx", index=False)
+            with get_openai_callback() as usage_info:
+                relevant_output = process_user_input(input)
+                total_input_tokens = usage_info.prompt_tokens
+                total_output_tokens = usage_info.completion_tokens
+                total_cost = usage_info.total_cost
+                update_usage_logs(Stage.MISCELLANEOUS.value, input, total_input_tokens, total_output_tokens, total_cost)
+            if relevant_output == 'irrelevant':
+                st.error('Irrelevant Output! Please input a relevant prompt')
+            else:
+                _, futures = filterExcel(uploaded_file,  input)
+                issues, results, numDone, numFutures, total_input_tokens, total_output_tokens, total_cost = [],[], 0, len(futures), 0, 0, 0
+                progessBar = st.progress(0, text="Article filtering in progress...")
+                for future in as_completed(futures):
+                    row = future.result()
+                    total_input_tokens += row[5]
+                    total_output_tokens += row[6]
+                    total_cost += row[7]
+                    results.append(row[0:5])
+                    numDone += 1
+                    progessBar.progress(numDone/numFutures, text="Article filtering in progress...")
+                
+                update_usage_logs(Stage.EXCEL_FILTERING.value, input, total_input_tokens,total_output_tokens,total_cost)
+                dfOut = pd.DataFrame(results, columns = ["DOI","TITLE","ABSTRACT","llmOutput", "jsonOutput"])
+                dfOut = getOutputDF(dfOut)
+                dfOut.to_excel("output/excel_result.xlsx", index=False)
 
-            st.session_state.filtered = input
-            st.experimental_rerun()
+                st.session_state.filtered = input
+                st.experimental_rerun()
 
 else:
     st.subheader("Prompt")
