3c9f8de265de6085542cd15264dc64f0239cd385
ashleyleerx <ashleyleeruoxin@gmail.com>
2023-10-27 10:53:14 +0800

    Update

--- pages/4_📗Excel Filtering.py
+++ pages/4_📗Excel Filtering.py
@@ -25,7 +25,8 @@ from update_cost import update_usage_logs, Stage
 
 
 st.set_page_config(layout="wide")
-add_logo("images/htpd_text.png", height=100)
+add_logo("images/temp_logo.png", height=100)
+
 
 st.markdown("<h1 style='text-align: left; color: Black;'>Excel Filtering</h1>", unsafe_allow_html=True)
 st.markdown('#')
